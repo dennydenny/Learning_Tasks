1725de4748d12636c1508369e24ad0b0c7f9712a
dennydenny <dennyhaliulin@gmail.com>
2016-07-12 12:01:49 -0700

    Add first task from Strings

---
+++ Daniel_Khaliulin_Strings/Daniel_Khaliulin_Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Daniel_Khaliulin_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приложение определяет среднюю длину слова по введённой текстовой строке");
+            Console.WriteLine("Пожалуйста, введите данные:");
+
+            String sentence = Console.ReadLine();
+            try
+            {
+                StringHelper sh = new StringHelper(sentence);
+                Console.WriteLine("Средняя длина слова {0} ", sh.AverageWordLenght);
+                Console.ReadKey();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла ошибка {0}", e.Message);
+                Console.ReadKey();
+            }
+
+           
+
+    }
+}
+}
---
+++ Daniel_Khaliulin_Strings/Daniel_Khaliulin_Strings/StringHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Daniel_Khaliulin_Strings
+{
+    class StringHelper
+    {
+        private String _sentence;
+        private int _averageWordLenght;
+        public int AverageWordLenght {
+            get {return CalculateAverageWordLenght();}
+            private set { _averageWordLenght = value; }
+        }
+
+        public StringHelper(String text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException("Text null or empty");
+            }
+            else
+            {
+                _sentence = text;
+            }
+        }
+
+        private int CalculateAverageWordLenght()
+        {
+            if (!String.IsNullOrEmpty(_sentence))
+            {
+                String[] words = _sentence.Split(new char[] { ',', ' ', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                int sentenseLenght = 0;
+                foreach (String s in words)
+                {
+                    Console.WriteLine(s);
+                    sentenseLenght += s.Length;
+                }
+                Console.WriteLine("sentenseLenght - {0} words.Length - {1}", sentenseLenght, words.Length);
+                return AverageWordLenght = sentenseLenght / words.Length; ;
+            }
+            else 
+            {
+                throw new ArgumentNullException("Text null or empty");
+            }
+        }
+    }
+}
