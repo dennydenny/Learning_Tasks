83a4a021dbe3501e6d778781a01990e37a05f5fe
dennydenny <dennyhaliulin@gmail.com>
2016-07-30 08:09:28 -0700

    Temporary commit of VFS task

---
+++ Architecture task/Architecture task/Folder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Architecture_task
+{
+    class Folder :  IFileSystemNode
+    {
+        // Имя папки.
+        public String Name { get; set; }
+
+        // Имя родителя.
+        public String ParentName { get; set; }
+
+        // Полный путь до папки.
+        public String FullPath
+        {
+            get
+            {
+                return ParentName + @"\" + Name;
+            }
+        }
+
+        // Тип узла???
+        public String Type
+        {
+            get
+            {
+                return "Folder";
+            }
+        }
+
+        // Список элементов в папке.
+        private List<Folder> _items = new List<Folder>();
+        public List<Folder> Items 
+        { 
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор, инициирующий экземпляр папки.
+        /// </summary>
+        /// <param name="name">Имя папки.</param>
+        /// <param name="parentName">Имя родительской папки.</param>
+        public Folder(String name, String parentName)
+        {
+            this.Name = name;
+            this.ParentName = parentName;
+        }
+    }
+}
---
+++ Architecture task/Architecture task/IFileSystemNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Architecture_task
+{
+    interface IFileSystemNode
+    {
+        String Name { get; set; }
+        String ParentName { get; set; }
+        String FullPath { get;  }
+        String Type { get;  }
+    }
+}
